3d87c498e22d14f139ad9019b994ee29c53e816b
Shalom Karr <Shalomkarrsphone@gmail.com>
2025-10-30 23:35:47 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}" // Tells Tailwind to scan all HTML and JS files in the root folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
